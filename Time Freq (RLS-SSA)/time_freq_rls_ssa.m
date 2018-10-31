61f98df8d00cf70143e7a5fd5817ca55f25d05a7
Mohammad Tariqul Islam <ponir.bd@hotmail.com>
2018-10-31 13:25:30 -0400

    Update time_freq_rls_ssa.m

--- Time Freq (RLS-SSA)/time_freq_rls_ssa.m
+++ Time Freq (RLS-SSA)/time_freq_rls_ssa.m
@@ -17,35 +17,6 @@ function [BPM, N_prev, d]= time_freq_rls_ssa(sig,BPM_00,srate,i,idnb, N_prev, AB
     ZData = clean_up(sig(5,:),srate);
     ZData = ZData/norm(ZData);
     
-%     figure();
-%     subplot(4,1,1), peri(PPG, 4, ABPM);
-%     set(gca,'XMinorTick','on','YMinorTick','off');
-%     set(gca,'yticklabel',[]);
-%     xlabel('(a)');
-%     title('Periodogram of PPG Signal after Preprocessing');
-%     
-%     subplot(4,1,2), peri(XData, 4, ABPM);
-%     set(gca,'XMinorTick','on','YMinorTick','on');
-%     set(gca,'yticklabel',[]);
-%     xlabel('(b)');
-%     title('Periodogram of X-axis Accelerometer Signal after Preprocessing');
-%     
-%     subplot(4,1,3), peri(YData, 4, ABPM);
-%     set(gca,'XMinorTick','on','YMinorTick','on');
-%     set(gca,'yticklabel',[]);
-%     title('Periodogram of Y-axis Accelerometer Signal after Preprocessing');
-%     ylabel('Amplitude');
-%     xlabel('(c)');
-%     
-%     subplot(4,1,4), peri(ZData, 4, ABPM);
-%     set(gca,'XMinorTick','on','YMinorTick','on');
-%     set(gca,'yticklabel',[]);
-%     title('Periodogram of Z-axis Accelerometer Signal after Preprocessing');
-%     xlabel('(d)');
-    %xlabel('Frequency Bins');
-    
-    
-    
     %figure, plot(ACC(1:180));
     L0 = length(BPM_00);
 
@@ -83,43 +54,6 @@ function [BPM, N_prev, d]= time_freq_rls_ssa(sig,BPM_00,srate,i,idnb, N_prev, AB
         EX = E2;
     else
         EX = E2+E3; %Adding
-    end
-    
-    pqz=0;
-    if (i == 85 || i == 16 || i==74 || i == 136) && pqz == 1
-        
-        if i == 16
-            figure(1);
-            subplot(411), periWithPrevious(E2, 4, ABPM, BPM_00(end));
-            figure(2);
-            subplot(411), periWithPrevious(E3, 4, ABPM, BPM_00(end));
-            figure(3);
-            subplot(411), periWithPrevious(EX, 4, ABPM, BPM_00(end));
-        end
-        if i == 74
-            figure(1);
-            subplot(412), periWithPrevious(E2, 4, ABPM, BPM_00(end));
-            figure(2);
-            subplot(412), periWithPrevious(E3, 4, ABPM, BPM_00(end));
-            figure(3);
-            subplot(412), periWithPrevious(EX, 4, ABPM, BPM_00(end));
-        end
-        if i == 85
-            figure(1);
-            subplot(413), periWithPrevious(E2, 4, ABPM, BPM_00(end));
-            figure(2);
-            subplot(413), periWithPrevious(E3, 4, ABPM, BPM_00(end));
-            figure(3);
-            subplot(413), periWithPrevious(EX, 4, ABPM, BPM_00(end));
-        end
-        if i == 136
-            figure(1);
-            subplot(414), periWithPrevious(E2, 4, ABPM, BPM_00(end));
-            figure(2);
-            subplot(414), periWithPrevious(E3, 4, ABPM, BPM_00(end));
-            figure(3);
-            subplot(414), periWithPrevious(EX, 4, ABPM, BPM_00(end));
-        end
     end
     
         
